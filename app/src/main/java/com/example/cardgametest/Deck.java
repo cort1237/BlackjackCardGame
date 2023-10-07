0af73ad7e8dc8ff3bf95a854e0b73c4cf1011b3a
Charles Ross <115509763+cmr190010@users.noreply.github.com>
2023-10-07 15:40:30 -0500

    initial Deck class

---
+++ app/src/main/java/com/example/cardgametest/Deck.java
@@ -0,0 +1,101 @@
+package com.example.cardgametest;
+
+import java.util.*;
+
+/*
+Create a Deck class that will shuffle 6 52-card and has the following functions:
+    retrieveTop(); - Retrieve and remove top card from deck
+    reset(); - Return all cards to deck and shuffle
+    getDeckSize(); - Retrieve the amount of cards in the deck
+*/
+public class Deck {
+    private List<Card> cards;
+
+    public Deck() {
+        initializeDeck();
+        shuffle();
+    }
+
+    private void initializeDeck() {
+        cards = new ArrayList<Card>();
+        for (int i = 0; i < 6; i++) { // Create 6 decks of 52 cards
+            for (int rank = 1; rank <= 13; rank++) {
+                int value = rank;
+                if (rank > 9) {
+                    value = 10;
+                }
+                for (String suit : Card.SUITS) {
+                    cards.add(new Card(rank, value, suit));
+                }
+            }
+        }
+    }
+
+    public Card retrieveTop(){
+        if (cards.isEmpty()) {
+            throw new IllegalStateException("Deck is empty.");
+        }
+        return cards.remove(0);
+    }
+
+    public void reset() {
+        cards.clear();
+        initializeDeck();
+        shuffle();
+    }
+
+    public int getDeckSize() {
+        return cards.size();
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void printCards() {
+        for (int i = 0; i<cards.size(); i++) {
+            System.out.println(cards.get(i).getRank() + " " + cards.get(i).getSuit() + " " + cards.get(i).getValue());
+        }
+    }
+}
+
+class Card {
+    public static final String[] SUITS = {"Clubs", "Diamonds", "Hearts", "Spades"};
+
+    private final int rank;
+    private final int value;
+    private final String suit;
+
+    public Card(int rank, int value, String suit) {
+        this.rank = rank;
+        this.value = value;
+        this.suit = suit;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getRank() {
+        switch (rank) {
+            case 1:
+                return "Ace";
+            case 11:
+                return "Jack";
+            case 12:
+                return "Queen";
+            case 13:
+                return "King";
+            default:
+                return Integer.toString(rank);
+        }
+    }
+}
